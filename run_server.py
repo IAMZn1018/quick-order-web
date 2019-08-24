0c6bc02ca5c05ea877e49c4f4aafca01a2cffb97
iamznn <200939540@qq.com>
2019-08-25 00:08:51 +0800

    update: json动态获取 新增404和500页面返回结果

--- run_server.py
+++ run_server.py
@@ -1,5 +1,4 @@
 from falcon_cors import CORS
-# from src.meal import *
 import falcon
 import json
 import os
@@ -8,10 +7,11 @@ from src.cors.meal import get_content
 
 class GetContent(object):
 
-    def on_get(self, req, resp):
+    def on_get(self, req, resp, filename):
         '''
         '''
-        menu = get_content()
+        menu = get_content(filename)
+
         result = {
             'code': 200,
             'content': {
@@ -64,12 +64,40 @@ class Suggestion(object):
         resp.body = json.dumps(result)
         resp.status = falcon.HTTP_200 
 
+def handle_404(req, resp):
+    result = {
+            'code': 404,
+            'content': {
+                'r': '请求路径错误'
+            },
+            'message': 'fail'
+        }
+
+    resp.status = falcon.HTTP_404
+    resp.body = json.dumps(result)
+
+def handle_500(req, resp):
+    result = {
+            'code': 500,
+            'content': {
+                'r': '服务器内部错误'
+            },
+            'message': 'fail'
+        }
+        
+    resp.status = falcon.HTTP_500
+    resp.body = json.dumps(result)
+
+
 public_cors = CORS(allow_all_origins=True,
                    allow_all_headers=True, allow_all_methods=True)
 
 application = falcon.API(middleware=[public_cors.middleware])
 application.req_options.auto_parse_form_urlencoded=True
 # router
-application.add_route('/api/content/', GetContent())
+application.add_route('/api/{filename}/', GetContent())
 application.add_route('/api/order/', Order())
 application.add_route('/api/suggestion/', Suggestion())
+
+application.add_sink(handle_404, '')
+application.add_sink(handle_500, '')
