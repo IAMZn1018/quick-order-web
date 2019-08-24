f396707b3991a3cd9095e84d22abe4e4fd560ed3
iamznn <200939540@qq.com>
2019-08-25 00:09:22 +0800

    json动态获取

--- src/cors/meal.py
+++ src/cors/meal.py
@@ -1,6 +1,10 @@
 import json
 
-def get_content():
-    f =  open('./src/config/xiangcun.json', encoding='utf-8')
+def get_content(filename):
+    try:
+        f =  open('./src/config/{}.json'.format(filename), encoding='utf-8')
+    except FileNotFoundError:
+        return None
+        
     json_data = json.load(f)
     return json_data
